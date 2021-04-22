e1d04e9673284572ae43519633370b2b4cb794b8
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-22 16:48:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,11 @@
 
 def position_taken?(array, i)
   
+<<<<<<< HEAD
   if array[i] == " " || array[i] == "" || array[i] == nil
+=======
+  if array[i] == " " || "" || nil
+>>>>>>> ea48bd1cf4ea4c9d16db27ed15268651e1974f5a
     false
   elsif array[i] == "X" || "O"
     true
